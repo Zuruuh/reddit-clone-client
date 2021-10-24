2aac97faaf84c2de8877005105a7353eda95d857
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-24 13:03:38 +0200

    :sparkles: Created Navbar with Auth Actions

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Box, Link, Flex, Button } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useLogoutMutation, useMeQuery } from "../../generated/graphql";
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps> = ({}) => {
+  const [{ data, fetching: loggedInFetching }] = useMeQuery();
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
+  let body: React.ReactElement | null = null;
+
+  // * Data is loading
+  if (loggedInFetching) {
+    // * User is not logged in
+  }
+  if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+    // * User is logged in
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data?.me?.username}</Box>
+        <Button
+          onClick={() => {
+            logout();
+          }}
+          isLoading={logoutFetching}
+          variant="link"
+        >
+          Log Out
+        </Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tan" p={4} ml={"auto"}>
+      <Box ml={"auto"}>{body ?? null}</Box>
+    </Flex>
+  );
+};
+export default Navbar;
